b01fec2deb7a40f0c3cc7ddfe2fa0fd8f135bd25
wesley_wisch <wesley_wisch@hotmail.com>
2022-11-22 10:38:59 -0300

    feat: Performing the responsive.

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { CiLogout } from 'react-icons/ci';
+import { FaBars } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 import logo from '../../assets/logo.png';
 import { sidebarItens } from '../../data/sidebarItens';
@@ -8,33 +10,57 @@ import './styles.css';
 
 export function Sidebar() {
   const [selected, setSelected] = useState(0);
+  const [expanded, setExpanded] = useState(false);
+
+  const sidebarVariants = {
+    true: {
+      left: '0',
+    },
+    false: {
+      left: '-60%'
+    }
+  }
 
   return (
-    <div className="sidebar-container">
-      <div className="logo">
-        <img src={logo} alt="Logo" />
-        <span>
-          Sh<span>o</span>ps
-        </span>
+    <>
+      <div
+        className='bars'
+        style={expanded ? { left: '60%' } : { left: '5%' }}
+        onClick={() => setExpanded(!expanded)}
+      >
+        <FaBars />
       </div>
 
-      <div className="menu">
-        {sidebarItens.map((item, key) => (
-          <div
-            key={key}
-            className={`menuItem ${selected === key && 'active'}`}
-            onClick={() => setSelected(key)}
-          >
-            {item.icon}
-            <span>{item.name}</span>
-          </div>
-        ))}
-
-        <div className="menuItem">
-          <CiLogout />
-          <span>Sign out</span>
+      <motion.div
+        className="sidebar-container"
+        variants={sidebarVariants}
+        animate={window.innerHeight <= 768 ? `${expanded}` : ''}
+      >
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+          <span>
+            Sh<span>o</span>ps
+          </span>
         </div>
-      </div>
-    </div>
+
+        <div className="menu">
+          {sidebarItens.map((item, key) => (
+            <div
+              key={key}
+              className={`menuItem ${selected === key && 'active'}`}
+              onClick={() => setSelected(key)}
+            >
+              {item.icon}
+              <span>{item.name}</span>
+            </div>
+          ))}
+
+          <div className="menuItem">
+            <CiLogout />
+            <span>Sign out</span>
+          </div>
+        </div>
+      </motion.div>
+    </>
   )
 }
